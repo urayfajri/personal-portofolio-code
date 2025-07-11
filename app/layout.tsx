897874a0d5d4a2e99a5ef5862a8faec165362019
urayfajri <uraywidiansyah88@gmail.com>
2025-07-11 21:34:49 +0700

    feat: prepare to deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,7 @@ export default function RootLayout({
         <div className="background-gradient absolute inset-0 -z-50 max-h-screen" />
         <div className="pointer-events-none absolute inset-0 -z-40 h-full bg-[url('/noisetexture.jpg')] opacity-20 mix-blend-soft-light"></div>
         <Footer />
+        {/* prepare to deploy */}
         <PrismicPreview repositoryName={repositoryName} />
       </body>
     </html>
