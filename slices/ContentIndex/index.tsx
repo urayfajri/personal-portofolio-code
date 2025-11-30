573e59ddd907961afbc5b5f08451b0e41e8f7451
Uray Muhamad Noor Fajri Widiansyah <77041388+urayfajri@users.noreply.github.com>
2025-11-30 18:50:43 +0700

    feat: add animation background (#3)

--- slices/ContentIndex/index.tsx
+++ slices/ContentIndex/index.tsx
@@ -1,10 +1,10 @@
-import { Content, isFilled } from "@prismicio/client";
-import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import { Content } from "@prismicio/client";
+import { SliceComponentProps } from "@prismicio/react";
 import { createClient } from "@/prismicio";
 import ContentList from "./ContentList";
 import Bounded from "@/components/Bounded";
-import Heading from "@/components/Heading";
 import { JSX } from "react";
+import AnimatedIntro from "./AnimatedIntro";
 /**
  * Props for `BlogPostIndex`.
  */
@@ -27,14 +27,10 @@ const BlogPostIndex = async ({
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      <Heading size="xl" className="mb-8">
-        {slice.primary.heading}
-      </Heading>
-      {isFilled.richText(slice.primary.description) && (
-        <div className="prose prose-xl prose-invert mb-10">
-          <PrismicRichText field={slice.primary.description} />
-        </div>
-      )}
+      <AnimatedIntro
+        heading={slice.primary.heading}
+        description={slice.primary.description}
+      />
       <ContentList
         items={items}
         contentType={slice.primary.content_type}
