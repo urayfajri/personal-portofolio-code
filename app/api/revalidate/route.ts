b277dac769d7b329b02ac9480a0c86b93e3a94d0
urayfajri <uraywidiansyah88@gmail.com>
2025-12-06 09:44:58 +0700

    fix version

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -2,7 +2,7 @@ import { revalidateTag } from "next/cache";
 import { NextResponse } from "next/server";
 
 export async function POST() {
-  await revalidateTag("prismic", "max");
+  await revalidateTag("prismic");
 
   return NextResponse.json({ revalidated: true, now: Date.now() });
 }
